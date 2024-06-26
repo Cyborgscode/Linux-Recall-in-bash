590acc28de08564c6d1afee68e0d1bcfcba03a2a
Cyborgscode <Cyborgscode@users.noreply.github.com>
2024-06-26 14:37:18 +0200

    Status changes are now correcly displayed

--- usr/sbin/recalltray.py
+++ usr/sbin/recalltray.py
@@ -8,90 +8,115 @@ gi.require_version('Gtk', '3.0')
 
 from gi.repository import Gtk as gtk, AppIndicator3 as appindicator
 
-indicator = appindicator.Indicator.new("customtray", "gtk-media-record", appindicator.IndicatorCategory.APPLICATION_STATUS)
-indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+restart = 1
+indicator = appindicator.Indicator.new("customtray", "gtk-media-pause", appindicator.IndicatorCategory.APPLICATION_STATUS)
 
 def main():
-
-  status = os.system("systemctl --user is-active recall.timer")
-  if status == 1:
-    indicator.set_menu(menu1())
-  else:
-    indicator.set_menu(menu2())
+  global indicator  
+  while restart == 1:
+#    print('Looping start')
+    status = os.system("systemctl --user is-active recall.timer 2>/dev/null")
+#    print("status=" + str(status))
+    if status != 0:
+      indicator = appindicator.Indicator.new("customtray", "gtk-media-pause", appindicator.IndicatorCategory.APPLICATION_STATUS)
+      indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+      indicator.set_menu(startmenu())
+    else:
+      indicator = appindicator.Indicator.new("customtray", "gtk-media-record", appindicator.IndicatorCategory.APPLICATION_STATUS)
+      indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+      indicator.set_menu(stopmenu())
   
-  gtk.main()
+    gtk.main()
+    
+  print('App beendet')
 
-def menu1():
+def startmenu():
   menu = gtk.Menu()
- 
+  label = gtk.Label("Recall")    
+  menu.add(label)
+
   command_one = gtk.MenuItem('Recall starten')
   command_one.connect('activate', start)
   menu.append(command_one)
 
-  command_drei = gtk.MenuItem('Suchen')
-  command_drei.connect('activate', search)
-  menu.append(command_drei)
+  return basemenu(menu)
 
-  command_two = gtk.MenuItem('Cache clear')
-  command_two.connect('activate', cacheclr)
-  menu.append(command_two)
-
-  command_vier = gtk.MenuItem('Cache Status')
-  command_vier.connect('activate', cachestatus)
-  menu.append(command_vier)
-
-  exittray = gtk.MenuItem('Tray beenden')
-  exittray.connect('activate', quit)
-  menu.append(exittray)
-  
-  menu.show_all()
-  return menu
-  
-def menu2():
+def stopmenu():
   menu = gtk.Menu()
  
   command_one = gtk.MenuItem('Recall beenden')
   command_one.connect('activate', kill)
   menu.append(command_one)
 
-  command_drei = gtk.MenuItem('Suchen')
-  command_drei.connect('activate', search)
-  menu.append(command_drei)
+  return basemenu(menu)
 
-  command_two = gtk.MenuItem('Cache clear')
-  command_two.connect('activate', cacheclr)
-  menu.append(command_two)
+def basemenu(menu):
 
-  command_vier = gtk.MenuItem('Cache Status')
-  command_vier.connect('activate', cachestatus)
-  menu.append(command_vier)
+  status = os.system("systemctl --user is-enabled recall.timer 2>/dev/null")
+  if status != 0:
+    command_er = gtk.MenuItem('aktiviere Recall')
+    command_er.connect('activate', enablerecall)
+    menu.append(command_er)
+  else:
+    command_dr = gtk.MenuItem('deaktiviere Recall')
+    command_dr.connect('activate', disablerecall)
+    menu.append(command_dr)
+
+  command_cc = gtk.MenuItem('Cache löschen')
+  command_cc.connect('activate', cacheclr)
+  menu.append(command_cc)
+
+  command_cs = gtk.MenuItem('Cache Status')
+  command_cs.connect('activate', cachestatus)
+  menu.append(command_cs)
+
+  command_search = gtk.MenuItem('Suchen')
+  command_search.connect('activate', search)
+  menu.append(command_search)
 
   exittray = gtk.MenuItem('Tray beenden')
   exittray.connect('activate', quit)
   menu.append(exittray)
-  
+
   menu.show_all()
   return menu
 
 def start(_):
+  global indicator
   os.system("systemctl --user start recall.timer")
-  indicator.set_menu(menu2())
-
+#  os.system("systemctl --user enable recall.timer")
+  indicator.set_menu(stopmenu())
+  gtk.main_quit()
+  
 def kill(_):
+  global indicator
   os.system("systemctl --user stop recall.timer")
-  indicator.set_menu(menu1())
+#  os.system("systemctl --user disable recall.timer")
+  indicator.set_menu(startmenu())
+  gtk.main_quit()
 
 def cacheclr(_):
   os.system("/usr/sbin/recall-cache-clear")
 
 def cachestatus(_):
   os.system("/usr/sbin/recall-cache-status")
+  
+def enablerecall(_):
+  os.system("systemctl --user enable --now recall.timer")
+  gtk.main_quit()
+  
+def disablerecall(_):
+  os.system("systemctl --user disable recall.timer")
+  gtk.main_quit()
 
 def search(_):
   os.system("/usr/sbin/recall-ui")
 
 def quit(_):
+  global restart
+  restart = 0
   gtk.main_quit()
+  
 
 if __name__ == "__main__":
   main()
